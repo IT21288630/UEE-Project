00fccac780fe5273e4b3c412cc23c20bfaca1e9a
IT21288630 <it21288630@my.sliit.lk>
2023-08-05 09:37:35 +0530

    Caregiver login

--- app/src/main/java/com/example/uee/activities/MainActivity.kt
+++ app/src/main/java/com/example/uee/activities/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.uee.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.uee.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("appPref", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        val btnCareGiver: Button = findViewById(R.id.btnCaregiver)
+
+        btnCareGiver.setOnClickListener {
+            editor.apply {
+                putString("userType", "caregiver")
+                apply()
+            }
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/uee/dataClasses/CareGiver.kt
@@ -0,0 +1,7 @@
+package com.example.uee.dataClasses
+
+data class CareGiver(
+    var userName: String? = null,
+    var password: String? = null,
+    var name: String? = null,
+)
