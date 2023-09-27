409332fc56bdcf55421b58a803442dd314312466
IT21288630 <it21288630@my.sliit.lk>
2023-09-27 14:38:53 +0530

    Caregiver profile

---
+++ app/src/main/java/com/example/uee/fragments/CaregiverProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.example.uee.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.uee.databinding.FragmentCaregiverProfileBinding
+import com.google.android.material.transition.platform.MaterialFadeThrough
+
+class CaregiverProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentCaregiverProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialFadeThrough()
+        exitTransition = MaterialFadeThrough()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCaregiverProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
