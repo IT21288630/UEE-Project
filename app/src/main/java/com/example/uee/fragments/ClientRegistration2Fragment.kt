a7c4a1e759f6825e13e37497a81f06a8ae9ddc92
IT21223594 <it21223594@my.sliit.lk>
2023-10-26 01:08:56 +0530

    client functionality stage 2. App Icon added

--- app/src/main/java/com/example/uee/fragments/ClientRegistration2Fragment.kt
+++ app/src/main/java/com/example/uee/fragments/ClientRegistration2Fragment.kt
@@ -2,6 +2,7 @@ package com.example.uee.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -18,6 +19,21 @@ import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+
 /**
  * A simple [Fragment] subclass.
  * Use the [ClientRegistration2Fragment.newInstance] factory method to
@@ -33,7 +49,9 @@ class ClientRegistration2Fragment : Fragment() {
     private var uContact : String? = null
     private var uAbout : String? = null
     private var proPic : String? = null
+    private lateinit var proPicView: ImageView
     val phonePattern = Regex("^\\d{10}\$")
+    val PICK_IMAGE_PERMISSION = 71
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,7 +78,41 @@ class ClientRegistration2Fragment : Fragment() {
         val location = view.findViewById<TextInputLayout>(R.id.regClientLocationField)
         val contactNo = view.findViewById<TextInputLayout>(R.id.regClientContactNoField)
         val about = view.findViewById<TextInputLayout>(R.id.regClientAboutField)
-        val proPicView = view.findViewById<ImageView>(R.id.imgViewRegClientDP)
+        proPicView = view.findViewById(R.id.imgViewRegClientDP)
+
+
+        // Image selection logic
+        proPicView.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1
+                    )
+                } else {
+                    val gallery = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                    )
+                    startActivityForResult(gallery, PICK_IMAGE_PERMISSION)
+                }
+            } else {
+                val gallery = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                )
+                startActivityForResult(gallery, PICK_IMAGE_PERMISSION)
+            }
+        }
+
+
+
+
 
         //Done button action
          doneBtn.setOnClickListener(){
@@ -91,6 +143,50 @@ class ClientRegistration2Fragment : Fragment() {
 
          }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_PERMISSION) {
+            val imageUri = data?.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(
+                    requireActivity().contentResolver,
+                    imageUri
+                )
+                // Set the bitmap to your ImageView
+                proPicView.setImageBitmap(bitmap)
+
+                // Upload the selected image to Firebase Storage
+                uploadImageToFirebase(imageUri)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun uploadImageToFirebase(imageUri: Uri?) {
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val imagesRef = storageRef.child("images/${imageUri?.lastPathSegment}")
+
+        val uploadTask = imagesRef.putFile(imageUri!!)
+
+        uploadTask.addOnSuccessListener {
+            // Get the download URL of the uploaded image
+            imagesRef.downloadUrl.addOnSuccessListener { uri ->
+                val downloadUrl = uri.toString()
+                // Now you can use this URL to update the ImageView
+                proPic = downloadUrl
+            }.addOnFailureListener {
+                // Handle any errors
+                Toast.makeText(activity, "Failed to retrieve download URL.", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            // Handle unsuccessful uploads
+            Toast.makeText(activity, "Image upload failed.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
